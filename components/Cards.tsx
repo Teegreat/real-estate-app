154b0970256b42f855abf5c1289647e8fa9ee64e
TeeGreat <toyinatofarati64@gmail.com>
2025-02-05 12:14:29 +0100

    flatlist implemented for home page

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -47,9 +47,9 @@ export const Card = ({ onPress }: Props) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 h-80 relative"
+      className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative"
     >
-      <View className="flex flex-row items-center px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
+      <View className="flex flex-row absolute items-center px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
         <Image source={icons.star} className="size-2.5" />
         <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
           4.4
